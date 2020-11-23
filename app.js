cc758d747fa669fd26eaba50ab682495c72f0c20
JimApostolou <apostolou_j@hotmail.com>
2020-11-23 17:01:43 +0200

    added delete button

--- app.js
+++ app.js
@@ -1,14 +1,16 @@
 class Notes {
 
-    constructor(titleNote,textNote) {
+    constructor(titleNote,textNote,counter) {
         this.titleNote = titleNote.value;
         this.textNote = textNote.value;
+        this.counter = counter;
     }
 
     createDiv() {
         //Creates the div
         const divNote = document.createElement('div');
         divNote.classList.add('note-taken');
+        divNote.id = this.counter;
         //Adds a h3 tag 
         const titleTag = document.createElement('h3');
         titleTag.innerText = this.titleNote.toUpperCase();
@@ -27,6 +29,16 @@ class Notes {
         };
         detailButton.classList.add('detail-btn');
         divNote.appendChild(detailButton);
+        //Adds delete button 
+        const deleteButton = document.createElement('button');
+        deleteButton.innerHTML = '<i class="fas fa-trash-alt"></i>';
+        deleteButton.onclick = function() {
+            const element_to_delete = document.getElementById(divNote.id);
+            console.log(element_to_delete);
+            element_to_delete.remove();
+        };
+        deleteButton.classList.add('delete-btn');
+        divNote.appendChild(deleteButton);
         //Puts everything on the "parent" Div
         notesTaken.appendChild(divNote);
     }
@@ -38,7 +50,10 @@ const inputNote = document.getElementById('content-textarea');
 const addButton = document.getElementById('submit-btn');
 const notesTaken = document.getElementById('notes-added');
 
+let i = 0;
+
 addButton.addEventListener('click', () => {
-    const notes = new Notes(inputTitle,inputNote);
+    i++;
+    const notes = new Notes(inputTitle,inputNote,i);
     notes.createDiv();
 });
